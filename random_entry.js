6851331a18f8f73ff7689f5846bb538fb6b26e99
Duluri\Ryan <ryanoftinellb@gmail.com>
2018-01-16 01:08:29 +1100

    Add links to random entries

---
+++ random_entry.js
@@ -0,0 +1,15 @@
+randomEntry();
+
+function randomEntry() {
+	var url = "searching.json";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var text = JSON.parse(this.responseText);
+      var rand = text['urls'][Math.floor(Math.random() * text['urls'].length)];
+      window.open(rand + '.html', '_self');
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
