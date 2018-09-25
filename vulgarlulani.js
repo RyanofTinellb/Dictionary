b87efb92336522e1da03cdd2ebacf8ca5a7935b4
Ryan Eakins <ryanoftinellb@gmail.com>
2018-09-25 16:58:46 +1000

    Apply evolution to all High words

--- vulgarlulani.js
+++ vulgarlulani.js
@@ -1,23 +1,23 @@
 function vulgarlulani() {
-  var url = "vulgarlulani.json";
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var text = JSON.parse(this.responseText);
-      display(text, "results");
-    }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
+    var url = "vulgarlulani.json";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var text = JSON.parse(this.responseText);
+            display(text, "results");
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
 
 // displays results as list
 // @param Array arr: results array
 function display(arr, id) {
-  let disp = '<ol>';
-  for (entry of arr) {
-    disp += `<li>${entry.join(' > ')}</li>\n`;
-  }
-  disp += '</ol>';
-  document.getElementById(id).innerHTML = disp;
+    let disp = '<ol>';
+    for (entry of arr) {
+        disp += `<li>${entry.join(' > ')}</li>\n`;
+    }
+    disp += '</ol>';
+    document.getElementById(id).innerHTML = disp;
 }
