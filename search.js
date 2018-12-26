08a97ee20a14c38cfd62dbdc85b20ec19a93b20a
Ryan Eakins <ryanoftinellb@gmail.com>
2018-12-26 22:08:14 +1100

    Change results to unordered lists

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ const BACKUP_URL = 'searching.json';
 const QUERY = window.location.href.split('?')[1];
 const SEARCH = document.getElementById('term');
 const RESULTS = document.getElementById('results');
-const LENGTH = entry => entry.length;
+const LOWER = word => word.toLowerCase()
 const MARKDOWN = {
     '&rsquo;': "'",
     '&#x294;': "''",
@@ -31,8 +31,10 @@ async function search() {
     } else {
         place(terms);
         results = [collate, pos_lang, lang_def]
-                   .map(fn => display(fn(data, terms)))
-                   .sort(LENGTH).filter(LENGTH).join('<br>');
+                   .map(fn => display(fn(data, terms.map(LOWER))))
+                   .sort((a, b) => a.length - b.length)
+                   .filter(entry => entry.length)
+                   .join('');
     }
     if (results.length) {
         RESULTS.innerHTML = results;
@@ -71,7 +73,7 @@ function backupDisplay(pages, data, terms) {
             RegExp(`(${term}|${capitalise(term)})`, 'g'));
     return `${
     !arr.length ? 'No matching entries found.' :
-         `<ol>${pages.map(page => {
+         `<ul>${pages.map(page => {
             let pagenum = page.page;
             let link = data.urls[pagenum];
             let name = data.names[pagenum];
@@ -79,7 +81,7 @@ function backupDisplay(pages, data, terms) {
                 linenum => highlight(regexes, data.sentences[linenum]));
             return `<li><a href="${link}">${name}</a>: ${
                 lines.join(' &hellip; ')}</li>`;
-    }).join('')}</ol>`}`;
+    }).join('')}</ul>`}`;
 }
 
 function highlight(terms, line) {
@@ -192,7 +194,7 @@ function includesSome(entry, terms) {
 
 function display(entries) {
     if (entries.length) {
-        return `<ol>${entries.map(createLine).join('')}</ol>`;
+        return `<ul>${entries.map(createLine).join('')}</ul>`;
     } else {
         return ''
     }
@@ -258,137 +260,3 @@ function getTerms() {
 function removeTrailingPlus(string) {
     return string.slice(-1) == '+' ? string.slice(0, -1) : string;
 }
-
-// function unique(arr) {
-//     return arr.filter(
-//         (item, pos, ary) => !pos || item !== ary[pos - 1]
-//     );
-// }
-//
-// function uniquePageNumbers(pages) {
-//     return pages.map(page => {
-//         page.lines = unique(page.lines.sort());
-//         return page;
-//     });
-// }
-//
-// function multiTermSearch(arr, terms, andButton) {
-//     let pages = [].concat(...terms.map(term => oneTermSearch(arr, term)));
-//     pages.sort((a, b) => a.page - b.page);
-//     let output = [];
-//     let current = undefined;
-//     for (let page of pages) {
-//         if (current && page.page === current.page) {
-//             current.lines.push(...page.lines);
-//             current.count++;
-//         } else {
-//             if (current) { output.push(current); }
-//             current = {page: page.page, lines: page.lines, count: 1};
-//         }
-//     }
-//     output = uniquePageNumbers(output);
-//     filter = andButton ? page => page.count === terms.length : page => true;
-//     return output.filter(filter);
-// }
-//
-// function oneTermSearch(arr, term) {
-//     pages = arr.terms[term];
-//     text = [];
-//     for (page in pages) {
-//         text.push({
-//             page: parseInt(page),
-//             lines: pages[page].map(line => parseInt(line))
-//         });
-//     }
-//     return text;
-// }
-//
-// // capitalises first letter
-// function capitalise(string) {
-//     if (string.length == 0) {
-//         return ''
-//     }
-//     if (string.startsWith('&rsquo;')) {
-//         return string.replace('&rsquo;', '&#x294;');
-//     } else {
-//         return string.charAt(0).toUpperCase() + string.slice(1);
-//     }
-// }
-//
-// function markdown(terms) {
-//     const MARKING = [
-//         ')a', '&agrave;',
-//         '()e', '&ecirc;',
-//         ')e', '&egrave;',
-//         ')i', '&igrave;',
-//         ')o', '&ograve;',
-//         ')u', '&ugrave;',
-//         '_o', '&#x14d;',
-//         '+h', '&#x2b0;',
-//         ',c', '&#x255;',
-//         ',n', '&#x14b;',
-//         "''", '&#x294;',
-//         "'", '&rsquo;',
-//         '$h', '&#x2b1;',
-//         '-i', '&#x268;',
-//         '=j', '&#x25f;',
-//         '$l', '&#x28e;',
-//         '$n', '&#x272;',
-//         '$r', '&#x279;',
-//         ',r', '&#x157;',
-//         '!e', '&#x259;',
-//         '-u', '&#x289;',
-//         '_u', '&#x16b;',
-//         '.', '&middot;'
-//     ]
-//     return terms.map(term => {
-//         for (i = 0; i < MARKING.length; i++) {
-//             term = term.split(MARKING[i]).join(MARKING[++i]);
-//         }
-//         return term;
-//     });
-// }
-//
-// function titleSearch(arr, terms, andButton) {
-//     let names = arr.names.map((elt, i) => ({
-//         name: elt,
-//         url: arr.urls[i],
-//         count: 0,
-//     }));
-//     terms.forEach(term => {
-//         names.forEach(name => {
-//             if (name.name.toLowerCase().includes(term)) { name.count++; }
-//         });
-//     });
-//     numTerms = terms.length;
-//     // filter = andButton ? name => name.count === terms.length : name =>
-//     filter = name => (andButton ? name.count === terms.length : name.count);
-//     names = names.filter(filter);
-//     return `<div class='title-results'><ul>${names.map(
-//         name => `<a href='${name.url}'>${name.name}</a></li>`
-//     ).join(';<br> ')}</ul></div>`;
-// }
-//
-// function display(pages, data, id, terms, andButton) {
-//     terms = markdown(terms);
-//     let regexes = terms.map(term =>
-//             RegExp(`(${term}|${capitalise(term)})`, 'g'));
-//     document.getElementById(id).innerHTML = `${titleSearch(data, terms, andButton)}${
-//     !arr.length ? terms.join(' ') + ' not found' :
-//          `<ol>${pages.map(page => {
-//             let pagenum = page.page;
-//             let link = data.urls[pagenum];
-//             let name = data.names[pagenum];
-//             let lines = page.lines.map(
-//                 linenum => highlight(regexes, data.sentences[linenum]));
-//             return `<li><a href='${link}'>${name}</a>: ${
-//                 lines.join(' &hellip; ')}</li>`;
-//     }).join('')}</ol>`}`;
-// }
-//
-// function highlight(terms, line) {
-//     terms.forEach(term => {
-//         line = line.replace(term, '<strong>$1</strong>');
-//     });
-//     return line;
-// }
