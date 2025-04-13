61bfb9099f8de36ff01ab4dc6e94cf5f966006fd
RyanofTinellb <ryanoftinellb@gmail.com>
2025-04-14 08:23:39 +1000

    Bugfix random_entry

--- data/scripts/random_entry.js
+++ data/scripts/random_entry.js
@@ -1,13 +1,19 @@
 randomEntry();
 
 const SEARCH = 'searching.json';
+const createUrl = text => `/lex/${sellCaps(text)}.html`
+const sellCaps = text => mapString(text, sellLetterCaps);
+const mapString = (str, fn) => 
+    str.split('').map((c, i) => fn(c, i, str)).join('');
 
-function createUrl(text) {
-    return `${sellCaps(text)}.html`
-}
+// Changes space to period, and adds dollar-signs before capitals
+const sellLetterCaps = letter =>
+    letter == letter.toLowerCase() ? 
+    letter == ' ' ? '.' : letter : `$${letter.toLowerCase()}`
+
 
 async function randomEntry() {
-    let data = await fetch('../data/assets/wordlist.json');
+    let data = await fetch('/data/assets/wordlist.json');
     data = await data.json();
 	data = Object.values(data);
     let rand = data[Math.floor(Math.random() * data.length)].t;
